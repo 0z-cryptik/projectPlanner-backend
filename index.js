13ca284b9841930dc4712c51d14744ddeea28751
Eniitan <enitanolawale0@gmail.com>
2024-04-02 21:38:47 +0100

    installed express session for session management

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-
 const port = process.env.PORT || 3002;
+const mongoose = require('mongoose')
+const session = require('')
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
