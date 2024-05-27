04a3ea3ad4cb08503262ac01d2702793e718d0a1
Eniitan <enitanolawale0@gmail.com>
2024-05-27 22:16:56 +0100

    trying to add API security

---
+++ controllers/middleware.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const User = require("../models/userSchema");
+
+module.exports = {
+  verifyRequest: async (req, res, next) => {
+    const { apiToken } = req.query;
+
+    const user = await User.findOne({ apiToken });
+
+    if (user._id === res.locals.currentUser._id) {
+      next();
+    } else {
+      res.status(500).json({ success: false, reason: "wrong token" });
+    }
+  }
+};
