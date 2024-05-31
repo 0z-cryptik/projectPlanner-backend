d21409935baf179d7aaee9bccf5909abaa000b27
Eniitan <enitanolawale0@gmail.com>
2024-05-31 22:12:26 +0100

    debugging

--- index.js
+++ index.js
@@ -43,6 +43,20 @@ app.use(
   })
 );
 
+app.use(function(request, response, next) {
+  if (request.session && !request.session.regenerate) {
+      request.session.regenerate = (cb) => {
+          cb()
+      }
+  }
+  if (request.session && !request.session.save) {
+      request.session.save = (cb) => {
+          cb()
+      }
+  }
+  next()
+})
+
 /*app.use(
   session({
     store: MongoStore.create({ mongoUrl: database }),
